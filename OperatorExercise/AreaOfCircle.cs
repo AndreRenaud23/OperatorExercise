33901173388a71ddce6b0096e1ccd1498bdab264
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-09-14 12:20:05 -0500

    Adding areaOfCircle

---
+++ OperatorExercise/AreaOfCircle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class AreaOfCircle
+    {
+        public static void Area()
+        {
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("What is the radius of your circle");
+            Console.WriteLine("----------------------------------------------");
+            decimal radius = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Enter the type of measurement: ");
+            Console.WriteLine("----------------------------------------------");
+            var measurementType = Console.ReadLine();
+            Console.WriteLine("----------------------------------------------");
+            decimal areaOfCircle = (radius * radius) * 22 / 7;
+            decimal perimeterOfCircle = (radius + radius) * 22 / 7;
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine($"The area of the circle is: {areaOfCircle} {measurementType}");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine($"The perimeter of the circle is: {perimeterOfCircle} {measurementType}");
+            Console.WriteLine("----------------------------------------------");
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,11 +1,23 @@
 ﻿
 
 
+using OperatorExercise;
+Console.WriteLine("----------------------------------------------");
 Console.WriteLine("Enter the first number: ");
+Console.WriteLine("----------------------------------------------");
 var a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("----------------------------------------------");
 Console.WriteLine("Enter the second number: ");
+Console.WriteLine("----------------------------------------------");
 var b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("----------------------------------------------");
 int quotient = a / b;
 int remainder = a % b;
 
 Console.WriteLine($"{a}/{b} is {quotient} remainder of {remainder}");
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine("----------------------------------------------");
+
+AreaOfCircle.Area();
