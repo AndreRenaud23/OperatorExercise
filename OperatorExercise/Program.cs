5974fa86bb364e19aa61ba24d59aad8d9b8f61ba
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-09-14 10:20:15 -0500

    First commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+Console.WriteLine("Enter the first number: ");
+var a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+var b = Convert.ToInt32(Console.ReadLine());
+int quotient = a / b;
+int remainder = a % b;
+
+Console.WriteLine($"{a}/{b} is {quotient} remainder of {remainder}");
